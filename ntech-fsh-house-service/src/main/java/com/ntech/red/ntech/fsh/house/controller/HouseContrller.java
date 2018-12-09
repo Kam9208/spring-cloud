c08c18541248adcdad2ea7a1e473f63edc0441af
Kam <ntech9208@hotmail.com>
2018-12-09 22:22:40 +0800

    RestTemplete结合Ribbon使用

--- ntech-fsh-house-service/src/main/java/com/ntech/red/ntech/fsh/house/controller/HouseContrller.java
+++ ntech-fsh-house-service/src/main/java/com/ntech/red/ntech/fsh/house/controller/HouseContrller.java
@@ -1,8 +1,12 @@
 package com.ntech.red.ntech.fsh.house.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ntech.red.ntech.fsh.house.po.HouseInfo;
 
 @RestController
 @RequestMapping("/house")
@@ -11,5 +15,15 @@ public class HouseContrller {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello";
+	}
+	
+	@GetMapping("/data")
+	public HouseInfo getData(@RequestParam("name") String name) {
+		return new HouseInfo(1L, "上海", "虹口", "东体小区");
+	}
+	
+	@GetMapping("/data/{name}")
+	public String getData2(@PathVariable("name") String name) {
+		return name;
 	}
 }
